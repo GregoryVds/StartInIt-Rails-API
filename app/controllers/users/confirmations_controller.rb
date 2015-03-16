6de30610e52eeb534b32e34ee5cd1e2e10c337ac
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2015-03-16 11:31:51 +0100

    Change redirect path after email confirmation

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,4 +1,8 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  
+  def getting_started
+  end
+
   # GET /resource/confirmation/new
   # def new
   #   super
@@ -22,7 +26,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # The path used after confirmation.
-  # def after_confirmation_path_for(resource_name, resource)
-  #   super(resource_name, resource)
-  # end
+  def after_confirmation_path_for(resource_name, resource)
+    confirmation_getting_started_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords', confirmations: 'users/confirmations' }
 
   namespace :api do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
+  end
+
+  devise_scope :user do
+    get '/confirmation-getting-started' => 'users/confirmations#getting_started', as: :confirmation_getting_started
   end
 
   root to: 'home#index'
