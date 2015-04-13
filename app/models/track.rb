8c4b3fb3f4f37632e092eaf7682f8a375a44b462
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2015-04-13 11:42:40 +0200

    Correct inverse of

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ActiveRecord::Base
   has_many :exercises, dependent: :destroy, inverse_of: :track
-  has_many :achievements, as: :achievable, dependent: :destroy, inverse_of: :exercise
+  has_many :achievements, as: :achievable, dependent: :destroy, inverse_of: :achievable
   validates_presence_of :description, :difficulty, :name
   validates :difficulty, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
