567b01dd46c80ba2cf20b607fdf3de547dfd797d
Gregory Vander Schueren <gregory.vanderschueren@gmail.com>
2015-04-28 11:58:22 +0200

    Add achieved_count on track index

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,4 +3,8 @@ class Track < ActiveRecord::Base
   has_many :achievements, as: :achievable, dependent: :destroy, inverse_of: :achievable
   validates_presence_of :description, :difficulty, :name
   validates :difficulty, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  def achievements_count(user)
+    Exercise.where(track_id: id).joins(:achievements).where("achievements.user_id = ?", user.id).count
+  end
 end
--- app/views/api/v1/tracks/_track.json.jbuilder
+++ app/views/api/v1/tracks/_track.json.jbuilder
@@ -1,1 +1,3 @@
 json.(track, :id, :description, :difficulty, :name, :pre_requisites)
+json.achieved_count  track.achievements_count(current_user)
+json.exercises_count track.exercises.count
